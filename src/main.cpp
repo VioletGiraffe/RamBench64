f8aa37f7ca9da0f44ab1c849b2f2be13ce55e4c7
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2023-03-11 15:36:30 +0200

    Code cleanup, aligned_alloc

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,55 @@
 #include <chrono>
+#include <iomanip>
 #include <iostream>
 #include <memory>
+
+#include <stdlib.h>
 #include <string.h>
 
 #include <emmintrin.h>
 #include <immintrin.h>
 
+inline void* malloc_aligned(size_t size, size_t alignment) noexcept
+{
+#ifdef _WIN32
+	return _aligned_malloc(size, alignment);
+#else
+	return aligned_alloc(alignment, size);
+#endif
+}
+
+#ifdef _WIN32
+#define free_aligned _aligned_free
+#else
+#define free_aligned free
+#endif
+
 struct Bench {
-	explicit Bench(const size_t megabytes = 100 * 1024 * 1024) noexcept :
-		_taskSize{ megabytes / sizeof(size_t)}
+	explicit Bench(const size_t megabytes = 100) noexcept :
+		_taskSizeBytes{ megabytes  * 1024 * 1024 },
+		_a{ reinterpret_cast<char8_t*>(malloc_aligned(_taskSizeBytes, 256 / 8)), &free_aligned },
+		_b{ reinterpret_cast<char8_t*>(malloc_aligned(_taskSizeBytes, 256 / 8)), &free_aligned }
 	{
-		_a = std::unique_ptr<size_t[]>(new size_t[_taskSize]);
-		_b = std::unique_ptr<size_t[]>(new size_t[_taskSize]);
-
-		if (((size_t)_a.get()) % 32 != 0 || ((size_t)_b.get()) % 32 != 0)
+		if (((size_t)_a.get()) % (256/8) != 0 || ((size_t)_b.get()) % (256 / 8) != 0)
+		{
 			::abort();
+		}
 
-		::memset(_a.get(), 0xE2, megabytes); // Init the memory - required for the OS to actually allocate all the pages!
-		::memset(_b.get(), 0x5F, megabytes);
+		::memset(_a.get(), 0xE2, _taskSizeBytes); // Init the memory - required for the OS to actually allocate all the pages!
+		::memset(_b.get(), 0x5F, _taskSizeBytes);
 	}
 
 	size_t runReadBenchmark() noexcept
 	{
 		const auto startTime = std::chrono::high_resolution_clock::now();
 
-		static constexpr size_t stride = 256 / 8 / sizeof(size_t);
+		static constexpr size_t stride = 256 / 8;
 
-		const auto* __restrict aPtr = _a.get();
-		const auto* __restrict bPtr = _b.get();
+		const char8_t* __restrict aPtr = _a.get();
+		const char8_t* __restrict bPtr = _b.get();
+
 		__m256i sum256 {0};
-		for (const auto* end = aPtr + _taskSize; aPtr != end; aPtr += stride, bPtr += stride)
+		for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride, bPtr += stride)
 		{
 			auto a256 = _mm256_load_si256(reinterpret_cast<const __m256i*>(aPtr));
 			auto b256 = _mm256_load_si256(reinterpret_cast<const __m256i*>(bPtr));
@@ -45,19 +65,20 @@ struct Bench {
 		if (us == 0)
 			return 0;
 
-		return _taskSize * sizeof(size_t) * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
+		return _taskSizeBytes * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
 	}
 
 	size_t runReadBenchmarkSSE2() noexcept
 	{
 		const auto startTime = std::chrono::high_resolution_clock::now();
 
-		static constexpr size_t stride = 128 / 8 / sizeof(size_t);
+		static constexpr size_t stride = 128 / 8;
 
-		const auto* __restrict aPtr = _a.get();
-		const auto* __restrict bPtr = _b.get();
+		const char8_t* __restrict aPtr = _a.get();
+		const char8_t* __restrict bPtr = _b.get();
+
 		__m128i sum{ 0 };
-		for (const auto* end = aPtr + _taskSize; aPtr != end; aPtr += stride, bPtr += stride)
+		for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride, bPtr += stride)
 		{
 			auto a128 = _mm_load_si128(reinterpret_cast<const __m128i*>(aPtr));
 			auto b128 = _mm_load_si128(reinterpret_cast<const __m128i*>(bPtr));
@@ -73,7 +94,7 @@ struct Bench {
 		if (us == 0)
 			return 0;
 
-		return _taskSize * sizeof(size_t) * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
+		return _taskSizeBytes * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
 	}
 
 	size_t runWriteBenchmark() noexcept
@@ -83,10 +104,10 @@ struct Bench {
 		auto* __restrict aPtr = _a.get();
 		auto* __restrict bPtr = _b.get();
 
-		static constexpr size_t stride = 256 / 8 / sizeof(size_t);
+		static constexpr size_t stride = 256 / 8;
 
 		__m256i sum256 = _mm256_set1_epi64x(_result);
-		for (const auto* end = aPtr + _taskSize; aPtr != end; aPtr += stride, bPtr += stride)
+		for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride, bPtr += stride)
 		{
 			_mm256_store_si256(reinterpret_cast<__m256i*>(aPtr), sum256);
 			_mm256_store_si256(reinterpret_cast<__m256i*>(bPtr), sum256);
@@ -97,7 +118,7 @@ struct Bench {
 		if (us == 0)
 			return 0;
 
-		return _taskSize * sizeof(size_t) * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
+		return _taskSizeBytes * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
 	}
 
 	size_t runWriteBenchmarkSSE2() noexcept
@@ -107,10 +128,10 @@ struct Bench {
 		auto* __restrict aPtr = _a.get();
 		auto* __restrict bPtr = _b.get();
 
-		static constexpr size_t stride = 128 / 8 / sizeof(size_t);
+		static constexpr size_t stride = 128 / 8;
 
 		__m128i sum = _mm_set1_epi64x(_result);
-		for (const auto* end = aPtr + _taskSize; aPtr != end; aPtr += stride, bPtr += stride)
+		for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride, bPtr += stride)
 		{
 			_mm_storeu_si128(reinterpret_cast<__m128i*>(aPtr), sum);
 			_mm_storeu_si128(reinterpret_cast<__m128i*>(bPtr), sum);
@@ -121,7 +142,7 @@ struct Bench {
 		if (us == 0)
 			return 0;
 
-		return _taskSize * sizeof(size_t) * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
+		return _taskSizeBytes * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
 	}
 
 	size_t runCopyBenchmark() noexcept
@@ -131,18 +152,32 @@ struct Bench {
 		auto* __restrict aPtr = _a.get();
 		auto* __restrict bPtr = _b.get();
 
-		/*static constexpr size_t stride = 256 / 8 / sizeof(size_t);
+		static constexpr size_t stride = 256 / 8;
 
-		for (const auto* end = aPtr + _taskSize; aPtr != end; aPtr += stride * 2, bPtr += stride * 2)
+		char8_t buffer2K[2048];
+
+		for (const auto* end = aPtr + _taskSizeBytes; aPtr != end;)
 		{
-			auto tmp1 = _mm256_load_si256(reinterpret_cast<__m256i*>(aPtr));
-			auto tmp2 = _mm256_load_si256(reinterpret_cast<__m256i*>(aPtr) + 1);
+			for (size_t i = 0; i != std::size(buffer2K); i += stride, aPtr += stride)
+			{
+				auto tmp1 = _mm256_load_si256(reinterpret_cast<__m256i*>(aPtr));
+				_mm256_store_si256(reinterpret_cast<__m256i*>(buffer2K + i), tmp1);
 
-			_mm256_store_si256(reinterpret_cast<__m256i*>(bPtr), tmp1);
-			_mm256_store_si256(reinterpret_cast<__m256i*>(bPtr) + 1, tmp2);
-		}*/
+				//auto tmp2 = _mm256_load_si256(reinterpret_cast<__m256i*>(aPtr) + 1);
 
-		memcpy(bPtr, aPtr, _taskSize * sizeof(size_t));
+				//_mm_prefetch(reinterpret_cast<const char*>(aPtr) + 1 * stride, _MM_HINT_NTA);
+
+				//_mm256_store_si256(reinterpret_cast<__m256i*>(bPtr) + 1, tmp2);
+			}
+
+			for (size_t i = 0; i != std::size(buffer2K); i += stride, bPtr += stride)
+			{
+				auto tmp1 = _mm256_load_si256(reinterpret_cast<__m256i*>(buffer2K + i));
+				_mm256_store_si256(reinterpret_cast<__m256i*>(bPtr), tmp1);
+			}
+		}
+
+		//memcpy(bPtr, aPtr, _taskSize * sizeof(size_t));
 
 		const auto endTime = std::chrono::high_resolution_clock::now();
 		const auto us = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
@@ -150,7 +185,7 @@ struct Bench {
 		if (us == 0)
 			return 0;
 
-		return _taskSize * sizeof(size_t) * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
+		return _taskSizeBytes * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
 	}
 
 	size_t runCopyBenchmarkSSE2() noexcept
@@ -160,9 +195,9 @@ struct Bench {
 		auto* __restrict aPtr = _a.get();
 		auto* __restrict bPtr = _b.get();
 
-		static constexpr size_t stride = 128 / 8 / sizeof(size_t);
+		static constexpr size_t stride = 128 / 8;
 
-		for (const auto* end = aPtr + _taskSize; aPtr != end; aPtr += stride * 2, bPtr += stride * 2)
+		for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride * 2, bPtr += stride * 2)
 		{
 			auto tmp1 = _mm_load_si128(reinterpret_cast<__m128i*>(aPtr));
 			auto tmp2 = _mm_load_si128(reinterpret_cast<__m128i*>(aPtr) + 1);
@@ -177,12 +212,12 @@ struct Bench {
 		if (us == 0)
 			return 0;
 
-		return _taskSize * sizeof(size_t) * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
+		return _taskSizeBytes * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
 	}
 
 	size_t taskSizeMib() const noexcept
 	{
-		return _taskSize * sizeof(size_t) / (1024 * 1024);
+		return _taskSizeBytes / (1024 * 1024);
 	}
 
 	auto result() const noexcept
@@ -191,16 +226,17 @@ struct Bench {
 	}
 
 private:
-	std::unique_ptr<size_t[]> _a;
-	std::unique_ptr<size_t[]> _b;
+	const size_t _taskSizeBytes;
 
-	const size_t _taskSize;
+	std::unique_ptr<char8_t, decltype(&free_aligned)> _a;
+	std::unique_ptr<char8_t, decltype(&free_aligned)> _b;
+
 	size_t _result = 0;
 };
 
 int main()
 {
-	Bench bench{1000 * 1024 * 1024};
+	Bench bench{1000};
 	std::cout << "Task size: " << bench.taskSizeMib() << " MiB (x2)" << "\n\n";
 	std::cout << std::fixed << std::setprecision(1);
 
