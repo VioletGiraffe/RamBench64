f7c389ee9836f99a62c0976f76ecf4ab6669e9a0
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2023-03-12 22:19:25 +0200

    Code cleanup; running benches for all supported SIMD instruction sets

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,8 @@ inline void* malloc_aligned(size_t size, size_t alignment) noexcept
 #endif
 
 struct Bench {
+	enum InstructionSet {SSE2, AVX, AVX2};
+
 	explicit Bench(const size_t megabytes = 100) :
 		_taskSizeBytes{ megabytes * 1024 * 1024 },
 		_a{ reinterpret_cast<char8_t*>(malloc_aligned(_taskSizeBytes, 256 / 8)), &free_aligned },
@@ -44,32 +46,62 @@ struct Bench {
 		::memset(_b.get(), 0xEE, _taskSizeBytes);
 	}
 
-	size_t runReadBenchmark()
+	size_t runReadBenchmark(const InstructionSet simdVersion)
 	{
 		const auto startTime = std::chrono::high_resolution_clock::now();
-
-		static constexpr size_t stride = 256 / 8;
 
 		const char8_t* __restrict aPtr = _a.get();
 		const char8_t* __restrict bPtr = _b.get();
 
-		__m256i sum256 {0};
-		for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride, bPtr += stride)
+		if (simdVersion == InstructionSet::AVX2 || simdVersion == InstructionSet::AVX)
 		{
-			auto a256 = _mm256_load_si256(reinterpret_cast<const __m256i*>(aPtr));
-			auto b256 = _mm256_load_si256(reinterpret_cast<const __m256i*>(bPtr));
+			static constexpr size_t stride = 256 / 8;
 
-			auto localSum256 = _mm256_add_epi64(a256, b256);
-			sum256 = _mm256_add_epi64(sum256, localSum256);
-		}
-		const auto endTime = std::chrono::high_resolution_clock::now();
-		const auto us = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
+			__m256i sum256 {0};
+			for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride, bPtr += stride)
+			{
+				auto a256 = _mm256_load_si256(reinterpret_cast<const __m256i*>(aPtr));
+				auto b256 = _mm256_load_si256(reinterpret_cast<const __m256i*>(bPtr));
+
+				auto localSum256 = _mm256_add_epi64(a256, b256);
+				sum256 = _mm256_add_epi64(sum256, localSum256);
+			}
 
 #ifdef _WIN32
-		_result = sum256.m256i_u64[0] + sum256.m256i_u64[1] + sum256.m256i_u64[2] + sum256.m256i_u64[3];
+			_result = sum256.m256i_u64[0] + sum256.m256i_u64[1] + sum256.m256i_u64[2] + sum256.m256i_u64[3];
 #else
-		_result = sum256[0] + sum256[1] + sum256[2] + sum256[3];
+			_result = sum256[0] + sum256[1] + sum256[2] + sum256[3];
 #endif
+		}
+		else if (simdVersion == InstructionSet::SSE2)
+		{
+			static constexpr size_t stride = 128 / 8;
+
+			__m128i sum{ 0 };
+			for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride * 2, bPtr += stride * 2)
+			{
+				_mm_prefetch(reinterpret_cast<const char*>(aPtr) + stride * 4, _MM_HINT_T0);
+
+				auto a128 = _mm_load_si128(reinterpret_cast<const __m128i*>(aPtr));
+				auto b128 = _mm_load_si128(reinterpret_cast<const __m128i*>(bPtr));
+
+
+				auto c128 = _mm_load_si128(reinterpret_cast<const __m128i*>(aPtr) + 1);
+				auto d128 = _mm_load_si128(reinterpret_cast<const __m128i*>(bPtr) + 1);
+
+				sum = _mm_add_epi64(sum, _mm_add_epi64(a128, b128));
+				sum = _mm_add_epi64(sum, _mm_add_epi64(c128, d128));
+			}
+
+#ifdef _WIN32
+			_result = sum.m128i_u64[0] + sum.m128i_u64[1];
+#else
+			_result = sum[0] + sum[1];
+#endif
+		}
+
+		const auto endTime = std::chrono::high_resolution_clock::now();
+		const auto us = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
 
 		// Verifying the result. The formula for sum of all consecutive numbers 1..N is n * (n + 1) / 2
 		const uint64_t N = _taskSizeBytes / sizeof(uint64_t) * 2 - 1 /* there is 0 */;
@@ -83,129 +115,64 @@ struct Bench {
 		return _taskSizeBytes * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
 	}
 
-	size_t runReadBenchmarkSSE2()
-	{
-		const auto startTime = std::chrono::high_resolution_clock::now();
-
-		static constexpr size_t stride = 128 / 8;
-
-		const char8_t* __restrict aPtr = _a.get();
-		const char8_t* __restrict bPtr = _b.get();
-
-		__m128i sum{ 0 };
-		for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride * 2, bPtr += stride * 2)
-		{
-			_mm_prefetch(reinterpret_cast<const char*>(aPtr) + stride * 4, _MM_HINT_T0);
-
-			auto a128 = _mm_load_si128(reinterpret_cast<const __m128i*>(aPtr));
-			auto b128 = _mm_load_si128(reinterpret_cast<const __m128i*>(bPtr));
-
-
-			auto c128 = _mm_load_si128(reinterpret_cast<const __m128i*>(aPtr) + 1);
-			auto d128 = _mm_load_si128(reinterpret_cast<const __m128i*>(bPtr) + 1);
-
-			sum = _mm_add_epi64(sum, _mm_add_epi64(a128, b128));
-			sum = _mm_add_epi64(sum, _mm_add_epi64(c128, d128));
-		}
-		const auto endTime = std::chrono::high_resolution_clock::now();
-		const auto us = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
-
-#ifdef _WIN32
-		_result = sum.m128i_u64[0] + sum.m128i_u64[1];
-#else
-		_result = sum[0] + sum[1];
-#endif
-
-		// Verifying the result. The formula for sum of all consecutive numbers 1..N is n * (n + 1) / 2
-		const uint64_t N = _taskSizeBytes / sizeof(uint64_t) * 2 - 1 /* there is 0 */;
-		const uint64_t expectedSum = N * (N + 1) / 2;
-		if (expectedSum != _result)
-			throw std::runtime_error("Result verification failed! Memory error or CPU instability?");
-
-		if (us == 0)
-			return 0;
-
-		return _taskSizeBytes * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
-	}
-
-	size_t runWriteBenchmark()
+	size_t runWriteBenchmark(const InstructionSet simdVersion)
 	{
 		const auto startTime = std::chrono::high_resolution_clock::now();
 
 		auto* __restrict aPtr = _a.get();
 		auto* __restrict bPtr = _b.get();
 
-		static constexpr size_t stride = 256 / 8;
-
-		const __m256i inc = _mm256_set1_epi64x(8);
-
-		__m256i valuesEven = _mm256_set_epi64x(0, 2, 4, 6);
-		__m256i valuesOdd = _mm256_set_epi64x(1, 3, 5, 7);
-		for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride, bPtr += stride)
+		if (simdVersion == InstructionSet::AVX2)
 		{
-			_mm256_stream_si256(reinterpret_cast<__m256i*>(aPtr), valuesEven);
-			valuesEven = _mm256_add_epi64(valuesEven, inc);
+			static constexpr size_t stride = 256 / 8;
 
-			_mm256_stream_si256(reinterpret_cast<__m256i*>(bPtr), valuesOdd);
-			valuesOdd = _mm256_add_epi64(valuesOdd, inc);
+			const __m256i inc = _mm256_set1_epi64x(8);
+
+			__m256i valuesEven = _mm256_set_epi64x(0, 2, 4, 6);
+			__m256i valuesOdd = _mm256_set_epi64x(1, 3, 5, 7);
+			for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride, bPtr += stride)
+			{
+				_mm256_stream_si256(reinterpret_cast<__m256i*>(aPtr), valuesEven);
+				valuesEven = _mm256_add_epi64(valuesEven, inc);
+
+				_mm256_stream_si256(reinterpret_cast<__m256i*>(bPtr), valuesOdd);
+				valuesOdd = _mm256_add_epi64(valuesOdd, inc);
+			}
 		}
-		const auto endTime = std::chrono::high_resolution_clock::now();
-		const auto us = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
-
-		if (us == 0)
-			return 0;
-
-		return _taskSizeBytes * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
-	}
-
-	size_t runWriteBenchmarkSSE2()
-	{
-		const auto startTime = std::chrono::high_resolution_clock::now();
-
-		auto* __restrict aPtr = _a.get();
-		auto* __restrict bPtr = _b.get();
-
-		static constexpr size_t stride = 128 / 8;
-
-		const __m128i inc = _mm_set1_epi64x(4);
-
-		__m128i valuesEven = _mm_set_epi64x(0, 2);
-		__m128i valuesOdd = _mm_set_epi64x(1, 3);
-		for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride, bPtr += stride)
+		else if (simdVersion == InstructionSet::AVX)
 		{
-			_mm_stream_si128(reinterpret_cast<__m128i*>(aPtr), valuesEven);
-			valuesEven = _mm_add_epi64(valuesEven, inc);
+			static constexpr size_t stride = 256 / 8;
 
-			_mm_stream_si128(reinterpret_cast<__m128i*>(bPtr), valuesOdd);
-			valuesOdd = _mm_add_epi64(valuesOdd, inc);
+			const __m256i inc = _mm256_set1_epi64x(8);
+
+			__m256i valuesEven = _mm256_set_epi64x(0, 2, 4, 6);
+			__m256i valuesOdd = _mm256_set_epi64x(1, 3, 5, 7);
+			for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride, bPtr += stride)
+			{
+				_mm256_store_si256(reinterpret_cast<__m256i*>(aPtr), valuesEven);
+				valuesEven = _mm256_add_epi64(valuesEven, inc);
+
+				_mm256_store_si256(reinterpret_cast<__m256i*>(bPtr), valuesOdd);
+				valuesOdd = _mm256_add_epi64(valuesOdd, inc);
+			}
 		}
-		const auto endTime = std::chrono::high_resolution_clock::now();
-		const auto us = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
-
-		if (us == 0)
-			return 0;
-
-		return _taskSizeBytes * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
-	}
-
-	size_t runCopyBenchmark()
-	{
-		const auto startTime = std::chrono::high_resolution_clock::now();
-
-		auto* __restrict aPtr = _a.get();
-		auto* __restrict bPtr = _b.get();
-
-		static constexpr size_t stride = 256 / 8;
-
-		for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride * 2, bPtr += stride * 2)
+		else if (simdVersion == InstructionSet::SSE2)
 		{
-			_mm_prefetch(reinterpret_cast<char*>(aPtr) + stride * 2, _MM_HINT_T0);
+			static constexpr size_t stride = 128 / 8;
 
-			_mm256_stream_si256(reinterpret_cast<__m256i*>(bPtr),     _mm256_load_si256(reinterpret_cast<__m256i*>(aPtr)));
-			_mm256_stream_si256(reinterpret_cast<__m256i*>(bPtr) + 1, _mm256_load_si256(reinterpret_cast<__m256i*>(aPtr) + 1));
+			const __m128i inc = _mm_set1_epi64x(4);
+
+			__m128i valuesEven = _mm_set_epi64x(0, 2);
+			__m128i valuesOdd = _mm_set_epi64x(1, 3);
+			for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride, bPtr += stride)
+			{
+				_mm_stream_si128(reinterpret_cast<__m128i*>(aPtr), valuesEven);
+				valuesEven = _mm_add_epi64(valuesEven, inc);
+
+				_mm_stream_si128(reinterpret_cast<__m128i*>(bPtr), valuesOdd);
+				valuesOdd = _mm_add_epi64(valuesOdd, inc);
+			}
 		}
-
-		//memcpy(bPtr, aPtr, _taskSizeBytes);
 
 		const auto endTime = std::chrono::high_resolution_clock::now();
 		const auto us = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
@@ -216,21 +183,48 @@ struct Bench {
 		return _taskSizeBytes * 2 /* A and B*/ * 1'000'000 / (1024 * 1024) / us;
 	}
 
-	size_t runCopyBenchmarkSSE2()
+	size_t runCopyBenchmark(const InstructionSet simdVersion)
 	{
 		const auto startTime = std::chrono::high_resolution_clock::now();
 
 		auto* __restrict aPtr = _a.get();
 		auto* __restrict bPtr = _b.get();
 
-		static constexpr size_t stride = 128 / 8;
-
-		for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride * 2, bPtr += stride * 2)
+		if (simdVersion == InstructionSet::AVX2)
 		{
-			_mm_prefetch(reinterpret_cast<char*>(aPtr) + stride * 2, _MM_HINT_T0);
+			static constexpr size_t stride = 256 / 8;
 
-			_mm_stream_si128(reinterpret_cast<__m128i*>(bPtr), _mm_load_si128(reinterpret_cast<__m128i*>(aPtr)));
-			_mm_stream_si128(reinterpret_cast<__m128i*>(bPtr) + 1, _mm_load_si128(reinterpret_cast<__m128i*>(aPtr) + 1));
+			for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride * 2, bPtr += stride * 2)
+			{
+				_mm_prefetch(reinterpret_cast<char*>(aPtr) + stride * 2, _MM_HINT_T0);
+
+				_mm256_stream_si256(reinterpret_cast<__m256i*>(bPtr), _mm256_load_si256(reinterpret_cast<__m256i*>(aPtr)));
+				_mm256_stream_si256(reinterpret_cast<__m256i*>(bPtr) + 1, _mm256_load_si256(reinterpret_cast<__m256i*>(aPtr) + 1));
+			}
+		}
+		else if (simdVersion == InstructionSet::AVX)
+		{
+			static constexpr size_t stride = 256 / 8;
+
+			for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride * 2, bPtr += stride * 2)
+			{
+				_mm_prefetch(reinterpret_cast<char*>(aPtr) + stride * 2, _MM_HINT_T0);
+
+				_mm256_store_si256(reinterpret_cast<__m256i*>(bPtr), _mm256_load_si256(reinterpret_cast<__m256i*>(aPtr)));
+				_mm256_store_si256(reinterpret_cast<__m256i*>(bPtr) + 1, _mm256_load_si256(reinterpret_cast<__m256i*>(aPtr) + 1));
+			}
+		}
+		else if (simdVersion == InstructionSet::SSE2)
+		{
+			static constexpr size_t stride = 128 / 8;
+
+			for (const auto* end = aPtr + _taskSizeBytes; aPtr != end; aPtr += stride * 2, bPtr += stride * 2)
+			{
+				_mm_prefetch(reinterpret_cast<char*>(aPtr) + stride * 2, _MM_HINT_T0);
+
+				_mm_stream_si128(reinterpret_cast<__m128i*>(bPtr), _mm_load_si128(reinterpret_cast<__m128i*>(aPtr)));
+				_mm_stream_si128(reinterpret_cast<__m128i*>(bPtr) + 1, _mm_load_si128(reinterpret_cast<__m128i*>(aPtr) + 1));
+			}
 		}
 
 		const auto endTime = std::chrono::high_resolution_clock::now();
@@ -261,13 +255,13 @@ private:
 	uint64_t _result = 0;
 };
 
-static float bestOfN(Bench& bench, size_t (Bench::* method)(), const size_t N)
+static float bestOfN(Bench& bench, size_t (Bench::* method)(Bench::InstructionSet), const Bench::InstructionSet simdVersion, const size_t N)
 {
 	float best = 0.0f;
 	for (size_t i = 0; i < N; ++i)
 	{
 		// Highest value (MiB/s) = best result
-		if (const auto result = (float)(bench.*method)(); result > best)
+		if (const auto result = (float)(bench.*method)(simdVersion); result > best)
 			best = result;
 	}
 
@@ -285,25 +279,36 @@ int main()
 		std::cout << "Task size: " << bench.taskSizeMib() << " MiB (x2)" << "\n\n";
 		std::cout << std::fixed << std::setprecision(1);
 
+		const auto runBenchmarkSetAndPrintResults = [&bench](Bench::InstructionSet simdVersion) {
+			std::cout << bestOfN(bench, &Bench::runWriteBenchmark, simdVersion, 30) / 1024.0f << " GiB/s\t";
+			std::cout << bestOfN(bench, &Bench::runReadBenchmark, simdVersion, 30) / 1024.0f << " GiB/s\t";
+			std::cout << bestOfN(bench, &Bench::runCopyBenchmark, simdVersion, 30) / 1024.0f << " GiB/s\t";
+		};
+
 		std::cout << "---------------------------------------------------" << '\n';
 		std::cout << "\tWrite\t\t" << "Read\t\t" << "Copy\t\t" << '\n';
 		std::cout << "---------------------------------------------------" << '\n';
 		std::cout << "AVX2\t";
 		if (cpuInfo.haveAVX2())
-		{
-			std::cout << bestOfN(bench, &Bench::runWriteBenchmark, 30) / 1024.0f << " GiB/s\t";
-			std::cout << bestOfN(bench, &Bench::runReadBenchmark, 30) / 1024.0f << " GiB/s\t";
-			std::cout << bestOfN(bench, &Bench::runCopyBenchmark, 30) / 1024.0f << " GiB/s\t";
-		}
+			runBenchmarkSetAndPrintResults(Bench::AVX2);
 		else
 			std::cout << "N/A\t\t" << "N/A\t\t" << "N/A\t\t";
+		std::cout << '\n';
 
+		std::cout << "AVX\t";
+		if (cpuInfo.haveAVX())
+			runBenchmarkSetAndPrintResults(Bench::AVX);
+		else
+			std::cout << "N/A\t\t" << "N/A\t\t" << "N/A\t\t";
 		std::cout << '\n';
 
 		std::cout << "SSE2\t";
-		std::cout << bestOfN(bench, &Bench::runWriteBenchmarkSSE2, 30) / 1024.0f << " GiB/s\t";
-		std::cout << bestOfN(bench, &Bench::runReadBenchmarkSSE2, 30) / 1024.0f << " GiB/s\t";
-		std::cout << bestOfN(bench, &Bench::runCopyBenchmarkSSE2, 30) / 1024.0f << " GiB/s\t";
+		if (cpuInfo.haveSSE2())
+			runBenchmarkSetAndPrintResults(Bench::SSE2);
+		else
+			std::cout << "N/A\t\t" << "N/A\t\t" << "N/A\t\t";
+		std::cout << '\n';
+
 		std::cout << '\n';
 		std::cout << "---------------------------------------------------" << '\n';
 
